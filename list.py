b9252b0a755769d50c8f1a4e2837362211940baf
sy2333 <sy2333gy@gmail.com>
2020-12-08 18:45:03 -0600

    import from pypi.org

--- list.py
+++ list.py
@@ -1,8 +1,15 @@
 import random
-data = [] 							#declare list
+import time
+import progressbar
+
+data = [] 
+count = 0 
+bar = progressbar.ProgressBar(max_value=1000000)							#declare list
 with open('reviews.txt', 'r') as s: #open reviews.txt
 	for line in s: 					#every entry in .txt
-		data.append(line) 			#put it in the list
+		data.append(line)			#put it in the list
+		count += 1
+		bar.update(count) 			
 print(len(data)) 					#length of the list
 
 sum_data = 0 						#declare sum_data
@@ -15,7 +22,35 @@ for d in data:
 	if len(d) < 100:
 		new.append(d)
 print('there are', len(new), 'reviews with less than 100 words')
-print(new[1])
+print(new[random.randint(1,1000)])
 
 good = [a for a in data if 'good' in a]
 print(good[random.randint(1,1000)])
+
+
+#word count
+start_time = time.time()
+wc = {}
+for d in data:
+	words = d.split(' ')
+	for word in words:
+		if word in wc:
+			wc[word] += 1
+		else:
+			wc[word] = 1
+
+for word in wc:
+	if wc[word] > 1000:
+		print(word,wc[word])
+end_time = time.time()
+print(end_time - start_time, 'secs')
+
+while True:
+	word = input('what word do you want to search: ')
+	if word == 'q':
+		break
+	if word in wc:
+		print(wc[word])
+	else:
+		print('word is not there')
+
